663c348137de6eea6562cd4ce95aa06a46bb295d
Patrick Lewis <pllewis1@gmail.com>
2015-11-01 23:51:57 -0600

    weekend homework

--- ajax.js
+++ ajax.js
@@ -1,53 +1,32 @@
 $.getJSON('https://api.github.com/users/pllewis1').done(function(data) {
   console.log(data);
   profileObj = data;
-  // profileObj.followers
-   $(".F").prepend("<h3>" + profileObj.followers + "</h3>")
-   $(".S").prepend("<h3>" + profileObj.followers + "</h3>")
-   $(".G").prepend("<h3>" + profileObj.following + "</h3>")
+   $(".F").html("<h3>" + profileObj.followers + '<br> <span class="blue">followers </span></h3>');
+   $(".S").html("<h3>" + profileObj.followers + '<br> <span class="blue">starred </span> </h3>');
+   $(".G").html("<h3>" + profileObj.following + '<br> <span class="blue"> following </span> </h3>');
+   $(".image_container").append("<img src=" + profileObj.avatar_url + ">");
+   $(".image_container:last-of-type").append("<h1>"+ profileObj.name + "</h1>")
+
+   $("h1").append("<h2>"+ profileObj.login + "</h2>")
 
 });
 $.getJSON('https://api.github.com/users/pllewis1/repos').done(function(data) {
   console.log(data);
-  // console.log(event.name);
-  reposObj = data;
+
   data.forEach(function(event){
     console.log(event.name);
 
     var container = $('<div class="repoman"> </div>');
-    var header = $('<h4 class="entry">' + event.name + '</h4>');
+    var header = $('<h4 class="entry"> <a href="#">' + event.name + '</a> <span>'  + event.language + '</span>' + '<span class="octicon octicon-star">' + event.stargazers_count + '</span>' + '<span class="octicon octicon-git-branch">'+ event.forks + '</span>'
+  +'</h4>');
     var time = $('<time>' + event.updated_at + '</time>');
 
-    // $(.repo).append(container);
-    // $(.repoman).append(header);
-    $('.repo').append(time)
+
     $(".repo").append(container);
     $('.repo').append(header);
+    $('.repo').append(time);
+
+
+
+    });
   });
-  });
-
-  // entries.forEach(function(entry){
-  //   var article = $('<article class="entry"></article>');
-  //   var header = $('<header>'+ entry.name +'</header>');
-  //   var content = $('<p>'+ entry.message +'</p>');
-  //   var time = $('<time>'+ entry.created +'</time>');
-  //
-  //   article.append(header);
-  //   article.append(content);
-  //   article.append(time);
-  //
-  //   log.prepend(article);
-  //
-  // });
-// }
-
-
-  //
-  // entries.forEach(function(entry){
-  //    var article = $('<article class="entry"></article>');
-  //    var header = $('<header>'+ entry.name +'</header>');
-  //    var content = $('<p>'+ entry.message +'</p>');
-  //    var time = $('<time>'+ entry.created +'</time>');
-
-
-// });
